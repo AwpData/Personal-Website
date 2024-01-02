ddce000805c43b9b50c975e17ec490d288684f2d
AwpData <awpdata@gmail.com>
2024-01-01 19:58:41 -0800

    Create header and main page, implement a dark mode toggler

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { React, useState } from 'react';
+import {Route, Routes} from 'react-router-dom';
+
+import {Header} from "./Header.js";
+
+import {ChakraProvider, VStack, Heading, Box, Text} from "@chakra-ui/react"
+import {SunIcon, MoonIcon} from "@chakra-ui/icons"
 
 function App() {
+  const [dark_mode_icon_background, set_dark_mode_icon_background] = useState("black")
+  const [dark_mode_icon, set_dark_mode_icon] = useState(<MoonIcon color="white"/>)
+  const [dark_mode_header, set_dark_mode_header] = useState("light-mode-header")
+  const [dark_mode_main, set_dark_mode_main] = useState("light-mode-main")
+
+function handle_dark_mode() {
+  set_dark_mode_icon_background(prevColor => (prevColor === "black" ? "white" : "black"));
+  set_dark_mode_header(prevHeader => (prevHeader === "dark-mode-header" ? "light-mode-header" : "dark-mode-header"));
+  set_dark_mode_main(prevMain => (prevMain === "dark-mode-main" ? "light-mode-main" : "dark-mode-main"));
+  set_dark_mode_icon(prevIcon => (prevIcon.type === MoonIcon ? <SunIcon color="black" /> : <MoonIcon color="white" />));
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Header dark_mode_color={dark_mode_icon_background} dark_mode_icon={dark_mode_icon} handle_dark_mode={handle_dark_mode} light_mode_header={dark_mode_header}/>
+      {/*Below is the main content for the home page*/}
+      <Box minH="100vh" className={dark_mode_main}>
+        <VStack align="start" pt="8" ml="8">
+          <Heading size="2xl">Hi there!</Heading>
+          <Text fontSize="xl">Test</Text>
+        </VStack>
+      </Box>
+    </ChakraProvider>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import {Button, Spacer, Heading, Box, Flex, IconButton} from "@chakra-ui/react"
+import {EmailIcon} from "@chakra-ui/icons"
+
+export function Header({dark_mode_color, dark_mode_icon, handle_dark_mode, light_mode_header}) {
+    return(
+    <Flex alignItems="center" justifyContent="center" h="100px" gap="8" boxShadow="0 4px 8px rgba(0, 0, 0, 0.2)" borderBottom="2px solid #ccc" className={light_mode_header}>
+        <Box p="8">
+          <p style={{ fontSize: '36px' }}>TREVOR<b>TANG</b></p>
+        </Box>
+        <Spacer />
+        <Box p="2">
+          <Heading size="md">HOME</Heading>
+        </Box>
+        <Box p="2">
+          <Heading size="md">PROJECTS</Heading>
+        </Box>
+        <Box p="2">
+          <Heading size="md">ABOUT</Heading>
+        </Box>
+        <Spacer />
+        <Button colorScheme="teal" leftIcon={<EmailIcon/>} size="lg" align="baseline"><Heading size="md">Contact</Heading></Button>
+        <IconButton 
+          size="lg"
+          colorScheme={dark_mode_color}
+          aria-label="dark mode toggle button"
+          icon={dark_mode_icon}
+          onClick={handle_dark_mode}
+          p="2"
+          m="8"
+          style={{ backgroundColor: dark_mode_color }} // Override the dark-mode class
+        />
+    </Flex>
+        )
+}
